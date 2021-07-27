8118394bd3a89d417bfebd3df07ffcf3e1fc04d7
ryan woods <federer5050@gmail.com>
2021-07-27 14:55:06 +0100

    adding code which works in old browsers

--- app.js
+++ app.js
@@ -2,27 +2,65 @@ const btns = document.querySelectorAll('.btn');
 const storeProducts = document.querySelectorAll('.store-product');
 // const search = document.getElementById(search);
 
-btns.forEach(function(button) {
 
-    button.addEventListener('click', (e) => {
-        e.preventDefault()
+// BUTTONS TO FILTER PRODUCTS
+
+// btns.forEach(function(button) {
+//     button.addEventListener('click', (e) => {
+//         e.preventDefault()
         
+//         const filter = e.target.dataset.filter;
+//         console.log(filter);
+        
+//         storeProducts.forEach((product)=> {
+//             if (filter === 'all'){
+//                 product.style.display = 'block'
+//             } else {
+//                 if (product.classList.contains(filter)){
+//                     product.style.display = 'block'
+//                 } else {
+//                     product.style.display = 'none'
+//                 }
+//             }
+//         });
+//     });
+// });
+
+
+
+// BUTTONS TO FILTER PRODUCTS (old browsers)
+
+// loop through all buttons
+for (i=0; i<btns.length; i++) {
+    // add click event to the button that is clicked
+    btns[i].addEventListener('click', function(e){
+        e.preventDefault();
+        
+        // get the value of the dataset 'filter' of the clicked button
         const filter = e.target.dataset.filter;
+        // console log data-filter value
         console.log(filter);
         
-        storeProducts.forEach((product)=> {
+        // loop through all products
+        for (i=0; i<storeProducts.length; i++) {
+            // if the clicked button attribute 'data-filter' has a value of 'all', then show all products
             if (filter === 'all'){
-                product.style.display = 'block'
+                storeProducts[i].style.display = 'block'
             } else {
-                if (product.classList.contains(filter)){
-                    product.style.display = 'block'
+                // 'classList.contains(filter)' checks if any product has a class the same as the 'data-filter' value of the clicked button
+                if (storeProducts[i].classList.contains(filter)){
+                    // whichever product(s) have the class, display them
+                    storeProducts[i].style.display = 'block'
                 } else {
-                    product.style.display = 'none'
+                    // product(s) which don't have the class, hide them
+                    storeProducts[i].style.display = 'none'
                 }
             }
-        });
+        };
     });
-});
+}
+
+
 
 // SEARCH FILTER
 const search = document.getElementById("search");
@@ -42,8 +80,6 @@ function filterProducts(e) {
     // console.log(productName[0]);
 
 
-
-
     // this loop works with IE
     for (i = 0; i < productName.length; i++) {
         const item = productName[i].firstChild.textContent;
@@ -54,6 +90,17 @@ function filterProducts(e) {
         }
     }
 
+
+       // This loop not compatible with IE
+
+    // productName.forEach(function(product) {
+    //     const item = product.firstChild.textContent;
+    //     if (item.toLowerCase().indexOf(text) != -1) {
+    //         product.parentElement.parentElement.style.display = "block"
+    //     } else {
+    //         product.parentElement.parentElement.style.display = "none"
+    //     }
+    // })
     
 
 
@@ -71,16 +118,7 @@ function filterProducts(e) {
 
 
 
-    // This loop not compatible with IE
-
-    // productName.forEach(function(product) {
-    //     const item = product.firstChild.textContent;
-    //     if (item.toLowerCase().indexOf(text) != -1) {
-    //         product.parentElement.parentElement.style.display = "block"
-    //     } else {
-    //         product.parentElement.parentElement.style.display = "none"
-    //     }
-    // })
+ 
 
 
 }
